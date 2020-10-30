83307042a70ab92944af4e14bb7456e33e6f5c68
Luka Baruskin <zumic96@gmail.com>
2020-10-30 21:35:26 +0100

    add check for 21 and above

--- bj.py
+++ bj.py
@@ -6,16 +6,13 @@ import random
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10}
+values = {'Ace':1, 'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10}
 
 class Card():
     def __init__(self,suit,rank):
         self.suit = suit
         self.rank = rank
-        if rank == 'Ace':
-            self.value = 0
-        else:
-            self.value = values[rank]
+        self.value = values[rank]
     def __str__(self):
         return self.rank + ' of ' + self.suit
 
@@ -51,6 +48,32 @@ player1 = Player("player1")
 for i in range(2):
     dealer.add_card(deck.deal_card())
     player1.add_card(deck.deal_card())
-# Check if anyone has 21
+# Start game loop
+game_alive = True
+while game_alive:
+    # Check if anyone has 21 or more and end game if that's the case
+    s_p1 = 0
+    for c in player1.all_cards:
+        s_p1 += c.value
+    if s_p1 == 21:
+        print("Player1 won!")
+        game_alive = False
+        break
+    elif s_p1 > 21:
+        print("Dealer won!")
+        game_alive = False
+        break
+    s_d = 0
+    for c in dealer.all_cards:
+        s_d += c.value
+    if s_d == 21:
+        print("Dealer won!")
+        game_alive = False
+        break
+    elif s_d > 21:
+        print("Player1 won!")
+        game_alive = False
+        break
+
 
 # Ask player if it wants to pull another one or is done
